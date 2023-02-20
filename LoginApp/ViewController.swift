2d37b21a371154ec930d9aa44de45d5913cb4047
ankit-verma-simform <ankit.verma@simformsolutions.com>
2023-02-20 15:24:01 +0530

    Initial Commit

---
+++ LoginApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Ankit Verma on 14/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: My section starts here ...
+    
+    @IBAction func btnLoginPressed(_ sender: UIButton) {
+        print("Success!")
+    }
+    
+}
+
